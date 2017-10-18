1e2f5c9f30e5242184524db7d7fe7c35b5e8fa9c
Adrian Li <li.adrianmc@gmail.com>
2017-10-18 17:34:16 -0400

    add web3 bridge to react frontend

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,7 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import getWeb3 from './utils/getWeb3';
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { web3: null }
+  }
+
+  componentDidMount() {
+    getWeb3.then(results => {
+      this.setState({ web3: results.web3 }, this.instantiateContract)
+    }).catch(() => {
+      console.log('Error finding web3.')
+    })
+  }
+
+  instantiateContract() {
+    // Confirm that web3 is available
+    console.log(this.state.web3)
+    
+    // TODO - instantiate the contract
+    console.log('TODO - Instatiating Contract')
+  }
+
   render() {
     return (
       <div className="App">
